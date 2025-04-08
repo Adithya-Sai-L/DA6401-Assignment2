83a1d4c501830d2341c0f2dc34963017f6be591a
Adithya-Sai-L <ns25z041@smail.iitm.ac.in>
2025-04-08 17:30:57 +0530

    Add augmentation to dataloader

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Build and experiment with CNN based image classifiers using a subset of the iNaturalist dataset
 
 The images are of different dimensions and the highest length and breadth of any image on both the train and test is 800. All the images will be resized to 256 x 256 through the default interpolation mode [(BILINEAR interpolation)](partA/dataloader.py#L11) and will become before being fed into the model.
+
+If Augmentation is set to True, the training dataloader will be include RandomHorizontalFlip with 0.5 probability and RandomRotation by 15 degrees [augmentations](partA/dataloader.py#L17)
--- partA/dataloader.py
+++ partA/dataloader.py
@@ -1,30 +1,58 @@
-import torch
-from torch.utils.data import DataLoader, Subset, random_split
+from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import Compose, Resize, ToTensor, InterpolationMode, RandomHorizontalFlip, RandomRotation
+from sklearn.model_selection import StratifiedShuffleSplit
+import numpy as np
 
-
-def get_training_dataloaders(data_dir, batch_size=32, num_workers=4, augmentaion=False):
+def get_training_dataloaders(data_dir, batch_size=32, num_workers=4, augmentation=False):
     """
     Returns train and validation DataLoaders with stratified sampling.
     """
-    transform = Compose([Resize((256, 256),interpolation=InterpolationMode.BILINEAR), ToTensor()])
-    full_dataset = ImageFolder(f"{data_dir}/train", transform=transform)
+
+    # Define transforms
+    base_transform = Compose([
+        Resize((256, 256), interpolation=InterpolationMode.BILINEAR),
+        ToTensor()
+    ])
+    
+    if augmentation:
+        train_transform = Compose([
+            RandomHorizontalFlip(p=0.5),
+            RandomRotation(degrees=15),
+            Resize((256, 256), interpolation=InterpolationMode.BILINEAR),
+            ToTensor()
+        ])
+    else:
+        train_transform = base_transform
+
+    # Load the same dataset twice with different transforms
+    train_full_dataset = ImageFolder(f"{data_dir}/train", transform=train_transform)
+    val_full_dataset = ImageFolder(f"{data_dir}/train", transform=base_transform)
+
+    # Get labels for stratified split
+    labels = [label for _, label in train_full_dataset.samples]
 
     # Stratified split based on class labels
-    train_size = int(0.8 * len(full_dataset))
-    val_size = len(full_dataset) - train_size
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    train_indices, val_indices = next(sss.split(np.zeros(len(labels)), labels))
 
-    train_dataset, val_dataset = random_split(
-        full_dataset,
-        [train_size, val_size],
-        generator=torch.Generator().manual_seed(42)
+    # Create train and validation subsets from the datasets with appropriate transforms
+    train_dataset = Subset(train_full_dataset, train_indices)
+    val_dataset = Subset(val_full_dataset, val_indices)
+
+    # Create data loaders
+    train_loader = DataLoader(
+        train_dataset, 
+        batch_size=batch_size,
+        shuffle=True, 
+        num_workers=num_workers
     )
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size,
-                              shuffle=True, num_workers=num_workers)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size,
-                            num_workers=num_workers)
+    
+    val_loader = DataLoader(
+        val_dataset, 
+        batch_size=batch_size,
+        num_workers=num_workers
+    )
 
     return train_loader, val_loader
 
